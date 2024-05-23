1c238282ff171fb2c45ea5cc19a8477fddc0e1a4
EduardoOliveira0133 <145940772+EduardoOliveira0133@users.noreply.github.com>
2024-05-23 01:16:40 -0300

    Add files via upload

---
+++ javascript/music-list.js
@@ -0,0 +1,128 @@
+let allMusic = [
+    {
+        name: "Lança Perfume",
+        artist: "Rita Lee",
+        img: "lancaperfume",
+        src: "lancaperfume"
+    },
+    {
+        name: "A Banda",
+        artist: "Chico Buarque",
+        img: "abanda",
+        src: "abanda"
+    },
+    {
+        name: "Tempo Perdido",
+        artist: "Legião Urbana",
+        img: "tempoperdido",
+        src: "tempoperdido"
+    },
+    {
+        name: "Chove Chuva",
+        artist: "Jorge Ben Jor",
+        img: "chovechuva",
+        src: "chovechuva"
+    },
+    {
+        name: "Apesar de Você",
+        artist: "Chico Buarque",
+        img: "calice",
+        src: "apesardevoce"
+    },
+    {
+        name: "João e Maria",
+        artist: "Chico Buarque",
+        img: "joaoemaria",
+        src: "joaoemaria"
+    },
+    {
+        name: "De Janeiro a Janeiro",
+        artist: "Roberta Campos",
+        img: "dejaneiroajaneiro",
+        src: "dejaneiroajaneiro"
+    },
+    {
+        name: "Já Sei Namorar",
+        artist: "Tribalistas",
+        img: "jaseinamorar",
+        src: "jaseinamorar"
+    },
+    {
+        name: "Cálice",
+        artist: "Chico Buarque",
+        img: "calice",
+        src: "calice"
+    },
+    {
+        name: "Geração Coca-Cola",
+        artist: "Legião Urbana",
+        img: "geracaococacola",
+        src: "geracaococacola"
+    },
+    {
+        name: "Onde Anda Você",
+        artist: "Toquinho, Vinícius de Moraes",
+        img: "ondeandavoce",
+        src: "ondeandavoce"
+    },
+    {
+        name: "Maluco Beleza",
+        artist: "Raul Seixas",
+        img: "malucobeleza",
+        src: "malucobeleza"
+    },
+    {
+        name: "Dias de Luta, Dias de Gloria",
+        artist: "Charlie Brown Jr.",
+        img: "diasdelutadiasdegloria",
+        src: "diasdelutadiasdegloria"
+    },
+    {
+        name: "Garota De Ipanema",
+        artist: "Tom Jobim",
+        img: "garotadeipanema",
+        src: "garotadeipanema"
+    },
+    {
+        name: "Amiga Da Minha Mulher",
+        artist: "Seu Jorge",
+        img: "amigadaminhamulher",
+        src: "amigadaminhamulher"
+    },
+    {
+        name: "Aquarela",
+        artist: "Toquinho",
+        img: "aquarela",
+        src: "aquarela"
+    },
+    {
+        name: "O Astronauta de Mármore",
+        artist: "Nenhum de Nós",
+        img: "oastronautademarmore",
+        src: "oastronautademarmore"
+    },
+    {
+        name: "O Descobridor Dos Sete Mares",
+        artist: "Tim Maia",
+        img: "odescobridordossetemares",
+        src: "odescobridordossetemares"
+    },
+    {
+        name: "Velha Infância",
+        artist: "Tribalistas",
+        img: "jaseinamorar",
+        src: "velhainfancia"
+    },
+    {
+        name: "Exagerado",
+        artist: "Cazuza",
+        img: "exagerado",
+        src: "exagerado"
+    },
+    {
+        name: "Águas de Março",
+        artist: "Elis Regina, Tom Jobim",
+        img: "aguasdemarco",
+        src: "aguasdemarco"
+    }
+]
